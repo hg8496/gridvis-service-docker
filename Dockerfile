2bcb21db4060ea3e9a112f75671a8761bc4d4ee3
Christian Stolz <christian.stolz@janitza.de>
2018-02-14 15:33:03 +0100

    Switched back to bash

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV HOME /root
 ENV VERSION 7.2.62-nightly-
 
 COPY response.varfile /response.varfile
-RUN apk add --no-cache openjdk8-jre fontconfig ttf-ubuntu-font-family wget gzip
+RUN apk add --no-cache openjdk8-jre fontconfig ttf-ubuntu-font-family wget gzip bash
 RUN VERSION=$VERSION$(date +%Y%m%d) \
     && wget -q -O service.sh http://gridvis.janitza.de/download/${VERSION}/GridVis-Service-${VERSION}-unix.sh \
     && sh service.sh -q -varfile /response.varfile \
--- gridvis-service.sh
+++ gridvis-service.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 FEATURE_PARAMS=''
-if [ $FEATURE_TOGGLES != "NONE" ]
+if [ "$FEATURE_TOGGLES" != "NONE" ]
 then
-    IFS=';'
-    for i in $FEATURE_TOOGLES; do
+    IFS=';' read -ra ADDR <<< "$FEATURE_TOGGLES"
+    for i in "${ADDR[@]}"; do
         FEATURE_PARAMS="$FEATURE_PARAMS -J-D${i}=true"
     done
 fi
 GROOVY_PARAM=''
-if [ $STARTUP_GROOVY != "NONE" ]                                              
+if [ -n "$STARTUP_GROOVY" -a "$STARTUP_GROOVY" != "NONE" ]
 then                                                                           
     GROOVY_PARAM="--groovy $STARTUP_GROOVY"                         
 fi
-exec /usr/local/GridVisService/bin/server -J-Duser.timezone=$USER_TIMEZONE --locale $USER_LANG -J-Dfile.encoding=$FILE_ENCODING $FEATURE_PARAMS $GROOVY_PARAM
+exec /usr/local/GridVisService/bin/server -J-Duser.timezone=${USER_TIMEZONE:-UTC} --locale ${USER_LANG:-en} -J-Dfile.encoding=${FILE_ENCODING:-UTF-8} $FEATURE_PARAMS $GROOVY_PARAM
