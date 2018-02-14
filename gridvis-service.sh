4fb6979703b8bcf32695b3ad56d0108dc025532e
janitza-chst <christian.stolz@janitza.de>
2018-02-14 13:22:38 +0100

    Enabled Feature Toogles for nightly

--- gridvis-service.sh
+++ gridvis-service.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-exec /usr/local/GridVisService/bin/server -J-Duser.timezone=$USER_TIMEZONE --locale $USER_LANG
+sudo own-volume
 
+FEATURE_PARAMS=''
+if [ $FEATURE_TOGGLES != "NONE" ]
+then
+    IFS=';' read -ra ADDR <<< "$FEATURE_TOGGLES"
+    for i in "${ADDR[@]}"; do
+        FEATURE_PARAMS="$FEATURE_PARAMS -J-D${i}=true"
+    done
+fi
+GROOVY_PARAM=''
+if [ $STARTUP_GROOVY != "NONE" ]                                              
+then                                                                           
+    GROOVY_PARAM="--groovy $STARTUP_GROOVY"                         
+fi
+exec /usr/local/GridVisService/bin/server -J-Duser.timezone=$USER_TIMEZONE --locale $USER_LANG -J-Dfile.encoding=$FILE_ENCODING $FEATURE_PARAMS $GROOVY_PARAM
