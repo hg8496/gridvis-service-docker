88f9427c3a09ec8f3f22857099fe525de02c23bb
Christian Stolz <hg8496@cstolz.de>
2021-04-08 11:46:18 +0200

    Delete cache for nightly builds

--- gridvis-service.sh
+++ gridvis-service.sh
@@ -23,4 +23,5 @@ fi
 Xvfb :1 -screen 0 800x600x24+32 -nolisten tcp -nolisten unix &
 export DISPLAY=:1
 export TZ=${USER_TIMEZONE:-UTC}
+rm -rf /opt/GridVisData/var/cache
 exec /usr/local/GridVisService/bin/server $ADD_PARAMS -J-Duser.timezone=${USER_TIMEZONE:-UTC} --locale ${USER_LANG:-en} -J-Dfile.encoding=${FILE_ENCODING:-UTF-8} $FEATURE_PARAMS $GROOVY_PARAM
